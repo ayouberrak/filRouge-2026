b6fcf9245dfd5d5a596d889b9fbbc6fea57ad812
ayouberrak <ayouberrak30@gmail.com>
2026-02-15 17:00:30 +0000

    Add DailyReport model

---
+++ app/Models/DailyReport.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DailyReport extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'date',
+        'absences_count',
+        'brief_status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
