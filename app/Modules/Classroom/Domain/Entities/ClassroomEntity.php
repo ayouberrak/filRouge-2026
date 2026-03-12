b7196cb54cbae3489444bbcbc1854c89e3e72d18
ayouberrak <ayouberrak30@gmail.com>
2026-03-12 21:43:31 +0000

    feat(Classroom): create rich domain entity

---
+++ app/Modules/Classroom/Domain/Entities/ClassroomEntity.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Modules\Classroom\Domain\Entities;
+
+class ClassroomEntity
+{
+    private ?int $id;
+    private string $name;
+
+    public function __construct(?int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function getId(): ?int { return $this->id; }
+    public function getName(): string { return $this->name; }
+}
