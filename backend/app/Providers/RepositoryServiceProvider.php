147d5e9aecc896a269d3d6857e5da56f67c14085
ayouberrak <ayouberrak30@gmail.com>
2026-03-18 20:39:47 +0000

    chore(activity): update RepositoryServiceProvider bindings

--- backend/app/Providers/RepositoryServiceProvider.php
+++ backend/app/Providers/RepositoryServiceProvider.php
@@ -16,8 +16,8 @@ use App\Modules\Squad\Infrastructure\Repositories\SquadRepository;
 use App\Modules\Brief\Domain\Repositories\BriefRepositoryInterface;
 use App\Modules\Brief\Infrastructure\Repositories\BriefRepository;
 
-use App\Modules\Deliverable\Domain\Repositories\DeliverableRepositoryInterface;
-use App\Modules\Deliverable\Infrastructure\Repositories\DeliverableRepository;
+use App\Modules\Livrable\Domain\Repositories\LivrableRepositoryInterface;
+use App\Modules\Livrable\Infrastructure\Repositories\LivrableRepository;
 
 use App\Modules\Activity\Domain\Repositories\ActivityRepositoryInterface;
 use App\Modules\Activity\Infrastructure\Repositories\ActivityRepository;
@@ -27,6 +27,7 @@ use App\Modules\Absence\Infrastructure\Repositories\AbsenceRepository;
 
 use App\Modules\Report\Domain\Repositories\DailyReportRepositoryInterface;
 use App\Modules\Report\Infrastructure\Repositories\DailyReportRepository;
+
 
 class RepositoryServiceProvider extends ServiceProvider
 {
@@ -39,10 +40,10 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(ClassroomRepositoryInterface::class, ClassroomRepository::class);
         $this->app->bind(SquadRepositoryInterface::class, SquadRepository::class);
         $this->app->bind(BriefRepositoryInterface::class, BriefRepository::class);
-        $this->app->bind(DeliverableRepositoryInterface::class, DeliverableRepository::class);
-        $this->app->bind(ActivityRepositoryInterface::class, ActivityRepository::class);
+        $this->app->bind(LivrableRepositoryInterface::class, LivrableRepository::class);
         $this->app->bind(AbsenceRepositoryInterface::class, AbsenceRepository::class);
         $this->app->bind(DailyReportRepositoryInterface::class, DailyReportRepository::class);
+        $this->app->bind(ActivityRepositoryInterface::class, ActivityRepository::class);
     }
 
     /**
