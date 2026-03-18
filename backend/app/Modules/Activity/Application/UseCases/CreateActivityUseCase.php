5f38a65b3b916ea70b7a86a2dae725f2b9668974
ayouberrak <ayouberrak30@gmail.com>
2026-03-18 20:38:59 +0000

    feat(activity): update CreateActivityUseCase to handle student assignments

---
+++ backend/app/Modules/Activity/Application/UseCases/CreateActivityUseCase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Modules\Activity\Application\UseCases;
+
+use App\Modules\Activity\Domain\Entities\ActivityEntity;
+use App\Modules\Activity\Domain\Repositories\ActivityRepositoryInterface;
+use App\Modules\Activity\Domain\ValueObjects\ActivityType;
+use App\Modules\Activity\Application\DTO\CreateActivityDTO;
+
+class CreateActivityUseCase
+{
+    private ActivityRepositoryInterface $repository;
+
+    public function __construct(ActivityRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function execute(CreateActivityDTO $dto): ActivityEntity
+    {
+        $activity = new ActivityEntity(
+            null,
+            $dto->title,
+            $dto->description,
+            new ActivityType($dto->type),
+            $dto->duration,
+            $dto->points,
+            $dto->formateurId,
+            $dto->classroomId
+        );
+
+        $savedActivity = $this->repository->save($activity);
+
+        if (!empty($dto->studentIds)) {
+            $this->repository->assignToStudents($savedActivity->getId(), $dto->studentIds);
+        }
+
+        return $savedActivity;
+    }
+}
