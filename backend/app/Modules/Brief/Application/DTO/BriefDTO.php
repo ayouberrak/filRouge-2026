dfb5a4f96376ee50480ce41873e3cdf34cc0b841
ayouberrak <ayouberrak30@gmail.com>
2026-03-18 20:42:31 +0000

    refactor(brief): update BriefDTO to link briefs with classrooms

--- backend/app/Modules/Brief/Application/DTO/BriefDTO.php
+++ backend/app/Modules/Brief/Application/DTO/BriefDTO.php
@@ -7,8 +7,14 @@ class BriefDTO
     public function __construct(
         public readonly string $title,
         public readonly string $description,
+        public readonly ?string $objectives,
         public readonly string $date_start,
         public readonly string $date_end,
+        public readonly string $difficulty,
+        public readonly string $modality,
+        public readonly string $status,
+        public readonly ?array $tags = [],
+        public readonly ?array $resources = [],
         public readonly ?string $file = null,
         public readonly ?int $formateur_id = null
     ) {}
