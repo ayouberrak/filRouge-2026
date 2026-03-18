303a768cfe1011c65cf77c160e935476b1b0b5b6
ayouberrak <ayouberrak30@gmail.com>
2026-03-18 20:42:53 +0000

    refactor(brief): update BriefRepositoryInterface.php

--- backend/app/Modules/Brief/Domain/Repositories/BriefRepositoryInterface.php
+++ backend/app/Modules/Brief/Domain/Repositories/BriefRepositoryInterface.php
@@ -2,11 +2,19 @@
 
 namespace App\Modules\Brief\Domain\Repositories;
 
+use App\Modules\Brief\Domain\Entities\BriefEntity;
+
 interface BriefRepositoryInterface
 {
-    public function findById(int $id);
-    public function findAll();
-    public function create(array $data);
-    public function update(int $id, array $data);
+    public function save(BriefEntity $brief): BriefEntity;
+
+    public function findById(int $id): ?BriefEntity;
+    
+    public function findByClassroomId(int $classroomId): array;
+
+    public function findAll(): array;
+    
     public function delete(int $id): bool;
+
+    public function assignClassrooms(int $briefId, array $classroomIds): void;
 }
