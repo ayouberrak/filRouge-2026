a308d0f72fc030376739685b915183478db3d377
ayouberrak <ayouberrak30@gmail.com>
2026-04-09 00:40:03 +0100

    chore: stabilized and formatted backend/app/Modules/Livrable/Application/DTO/SubmitLivrableDTO.php

--- backend/app/Modules/Livrable/Application/DTO/SubmitLivrableDTO.php
+++ backend/app/Modules/Livrable/Application/DTO/SubmitLivrableDTO.php
@@ -8,16 +8,19 @@ class SubmitLivrableDTO
     public readonly ?int $studentId;
     public readonly ?int $squadId;
     public readonly string $link;
+    public readonly ?string $message;
 
     public function __construct(
         int $briefId,
         ?int $studentId,
         ?int $squadId,
-        string $link
+        string $link,
+        ?string $message = null
     ) {
         $this->briefId = $briefId;
         $this->studentId = $studentId;
         $this->squadId = $squadId;
         $this->link = $link;
+        $this->message = $message;
     }
 }
