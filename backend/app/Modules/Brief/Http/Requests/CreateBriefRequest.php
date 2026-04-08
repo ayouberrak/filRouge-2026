136bf45932d2f21d4e632a0cd5f66c018dcf49dd
ayouberrak <ayouberrak30@gmail.com>
2026-04-09 00:39:57 +0100

    chore: stabilized and formatted backend/app/Modules/Brief/Http/Requests/CreateBriefRequest.php

--- backend/app/Modules/Brief/Http/Requests/CreateBriefRequest.php
+++ backend/app/Modules/Brief/Http/Requests/CreateBriefRequest.php
@@ -8,22 +8,30 @@ class CreateBriefRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true; // Autorisaton gérée par le middleware
+        return true; 
     }
 
     public function rules(): array
     {
         return [
             'title' => 'required|string|min:5|max:255',
+            'image_url' => 'nullable|string',
             'description' => 'required|string',
-            'objectives' => 'nullable|string',
+            'context' => 'nullable|string',
+            'objectives' => 'nullable|array',
             'date_start' => 'required|date',
             'date_end' => 'required|date|after_or_equal:date_start',
             'difficulty' => 'nullable|in:EASY,MEDIUM,HARD',
             'modality' => 'nullable|in:INDIVIDUAL,GROUP',
+            'pedagogical_modalities' => 'nullable|string',
+            'evaluation_modalities' => 'nullable|string',
             'status' => 'nullable|in:DRAFT,PUBLISHED,IN_PROGRESS,COMPLETED,ARCHIVED',
+            'points' => 'nullable|integer',
             'tags' => 'nullable|array',
             'resources' => 'nullable|array',
+            'deliverables' => 'nullable|array',
+            'performance_criteria' => 'nullable|array',
+            'target_competencies' => 'nullable|array',
         ];
     }
 }
